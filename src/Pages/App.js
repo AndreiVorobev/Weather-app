6c597e99b936fe706834f75445ebb1426df5d1a8
Andrei Vorobev <avorobev.exceedteam@gmail.com>
2021-12-02 09:34:03 +0300

    changes

--- src/Pages/App.js
+++ src/Pages/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import AuthPage from "./AuthPage";
+import LoginPage from "./LoginPage";
 import ProfilePage from "./ProfilePage";
 import WeatherPage from "./WeatherPage";
 
@@ -19,7 +20,8 @@ class App extends React.Component {
   render() {
     return (
       <Routes>
-        <Route path="/" element={<AuthPage />}></Route>
+        <Route path="/" element={<LoginPage />}></Route>
+        <Route path="/authpage" element={<AuthPage />}></Route>
         <Route path="/profilepage" element={<ProfilePage />}></Route>
         <Route path="/weatherpage" element={<WeatherPage />}></Route>
       </Routes>

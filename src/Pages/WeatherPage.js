1f645f88281e61e9554f86947f32951fa765771b
Andrei Vorobev <avorobev.exceedteam@gmail.com>
2021-12-09 10:47:02 +0300

    new PrivateRoutes

--- src/Pages/WeatherPage.js
+++ src/Pages/WeatherPage.js
@@ -1,7 +1,19 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function WeatherPage() {
+  // let navigate = useNavigate();
+  // useEffect(() => {
+  //   let authToken = sessionStorage.getItem("Auth Token");
+
+  //   // if (authToken) {
+  //   //   navigate("/");
+  //   // }
+
+  //   if (!authToken) {
+  //     navigate("/loginpage");
+  //   }
+  // }, [navigate]);
   return (
     <div>
       <h1>Weather page is here</h1>
